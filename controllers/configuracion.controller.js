d782495a45fc6fb9332fe94720d842201d8677c8
Jesus0204 <a01705442@tec.mx>
2024-03-20 23:11:02 -0600

    Agregar configuracion y ejs primitivo

--- controllers/configuracion.controller.js
+++ controllers/configuracion.controller.js
@@ -1,17 +1,17 @@
 const Periodo = require('../models/periodo.model');
 
 exports.get_configuracion = (request, response, next) => {
-    response.render('configuracion');
+    response.render('configuracion/configuracion');
 };
 
 exports.get_periodos = (request, response, next) => {
     Periodo.fetchAll()
-    .then(([rows, fieldData]) => {
-        response.render('periodos', {
-            periodos: rows
-        });
-    })
-    .catch((error) => {
-        console.log(error);
-    })
+        .then(([rows, fieldData]) => {
+            response.render('configuracion/periodos', {
+                periodos: rows
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        })
 };
